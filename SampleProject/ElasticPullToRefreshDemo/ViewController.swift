9cd8df7971faafbb23d02113831e23311f3c2383
Joshua Tessier <joshpc@gmail.com>
2016-01-18 14:12:28 +0900

    Fixes animation issue where the pull to refresh header would get out of place
    When pulling the table view, we properly position the pull to refresh header so that it seems as if it's being pulled out of the navigation bar. However, when it's animating down, it gets into a state where the shape of the view will not change as the animation is still in progress. Therefore we only begin observing the contentInset changes again once the animation is done, avoiding the problem all together.

--- SampleProject/ElasticPullToRefreshDemo/ViewController.swift
+++ SampleProject/ElasticPullToRefreshDemo/ViewController.swift
@@ -13,13 +13,29 @@ class ViewController: UIViewController {
 		let tableView = UITableView()
 		let wrapper = ElasticPullToRefresh(scrollView: tableView)
 		wrapper.didPullToRefresh = {
-			dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(5 * NSEC_PER_SEC)), dispatch_get_main_queue()) { () -> Void in
+			dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(2 * NSEC_PER_SEC)), dispatch_get_main_queue()) { () -> Void in
 				wrapper.didFinishRefreshing()
 			}
 		}
 		view = wrapper
 	}
 	
+	override func viewWillAppear(animated: Bool) {
+		super.viewWillAppear(animated)
+		
+		self.navigationItem.title = "Bananas"
+		self.navigationController?.navigationBar.titleTextAttributes = [
+			NSForegroundColorAttributeName : UIColor.whiteColor(),
+			NSFontAttributeName : UIFont.systemFontOfSize(18.0, weight: UIFontWeightBold)
+		];
+		self.navigationController?.navigationBar.translucent = false
+		self.navigationController?.navigationBar.shadowImage = UIImage()
+		self.navigationController?.navigationBar.setBackgroundImage(UIImage(), forBarMetrics: .Default)
+		self.navigationController?.navigationBar.barTintColor = UIColor.purpleColor()
+	}
+}
+
+class NavigationController: UINavigationController {
 	override func preferredStatusBarStyle() -> UIStatusBarStyle {
 		return .LightContent
 	}
